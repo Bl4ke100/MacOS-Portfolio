dccdf9fa6d93821811c2724755461971e2f0f4f1
Bl4ke100 <bl4ke100@gmail.com>
2026-04-29 20:43:46 +0530

    Improving the Spotify widget

--- src/windows/Spotify.jsx
+++ src/windows/Spotify.jsx
@@ -316,7 +316,7 @@ const LiveView = ({ liveTrack, stats }) => {
 };
 
 const StatsView = ({ stats }) => (
-    <div className="pb-60"> {/* Increased padding so play bar doesn't overlap */}
+    <div className="pb-80"> {/* Increased padding so play bar doesn't overlap */}
         {/* Hero gradient header */}
         <div
             className="px-8 pt-20 pb-8"
